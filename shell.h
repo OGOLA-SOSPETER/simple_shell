da1e5e1c62e8671df49ca30b28da82d203ed02a4
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-08-06 09:40:38 -0700

    shell updated header files

--- shell.h
+++ shell.h
@@ -108,5 +108,7 @@ char *error_1(char **args);
 char *error_2_exit(char **args);
 char *error_2_cd(char **args);
 char *error_2_syntax(char **args);
+char *error_126(char **args);
+char *error_127(char **args);
 
 #endif
